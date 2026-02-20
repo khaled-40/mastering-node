133e3f62aa6ee3ef89a99ffdc1b88e6d146a1006
khaled-40 <khaledmasud.dev@gmail.com>
2026-02-20 23:04:37 +0600

    fixed the order flow inspite of this being asynchronus

--- callback.js
+++ callback.js
@@ -1,14 +1,23 @@
-const processOrder = (order) => {
-    console.log(`processing order for ${order}`)
+const takeOrder = (customer, callback) => {
+    console.log(`take order for ${customer}`)
+    callback(customer)
+}
+const processOrder = (customer, callback) => {
+    console.log(`processing order for ${customer}`)
 
     setTimeout(() => {
-        console.log(`${order} is cooking`)
-        console.log(`${order} has been processed`)
+        console.log(`order is cooking`)
+        console.log(`order has been processed for ${customer}`)
+        callback(customer)
     }, 3000);
-
-    
 }
 
-console.log('Taking order for lobster');
-processOrder('lobstar')
-console.log('finished the order')
+const completeOrder = (customer) => {
+    console.log(`order completed for ${customer}`)
+}
+
+takeOrder('Shafiq', (customer) => {
+    processOrder(customer, (customer) => {
+        completeOrder(customer)
+    })
+})
